8bb01956d6b25d8c7b54869a0ba92f9db54855fe
patrickthesailorman <patrickthesaiorman@gmail.com>
2017-01-06 15:24:50 -0500

    vote button hidden from author

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -86,6 +86,6 @@ class CommentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def comment_params
-      params.require(:comment).permit(:post_id, :content, :user_id)
+      params.require(:comment).permit(:post_id, :votes, :content, :user_id)
     end
 end
