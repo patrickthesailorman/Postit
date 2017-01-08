f0634c64c68f5c76ff4fdaeb477fd3dc94f75006
patrickthesailorman <patrickthesaiorman@gmail.com>
2017-01-08 18:08:11 -0500

    password character minimum to 6

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   has_many :comments
   validates :username, presence: true
   validates :password, presence: true,
-              length: { minimum: 8 }
+              length: { minimum: 6 }
   validates :email, presence: true
   acts_as_voter
 end
